8c8a9caa7eed5fc65bc1bcae3d7ac42d5c0e92b8
KVCobbs <kenyacobbs9001@gmail.com>
2021-11-02 14:11:02 +0000

    one and two

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 //1
-function areBothSame(){
-  
+function areBothSame(x, y){
+  if(x == y) {
+      console.log('true')
+  } else 
+      console.log('false')
 }
 
 //2
 function areBothEqual(){
-  
+  if( x === y) {
+      console.log('true')
+  } else 
+  console.log('false')
 }
 
 //3
@@ -25,3 +31,4 @@ function areAllFourSameOrEqual(){
 }
 
 
+//areBothSame(4, 6)
